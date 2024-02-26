bd0e8dcfe297e2547c37327b07567416c1671533
felixasela <80848235+felixasela@users.noreply.github.com>
2024-02-26 16:12:10 -0500

    feat: API users

---
+++ src/models/personModel.py
@@ -0,0 +1,25 @@
+from database.db import get_connection
+from .entities.person import person
+
+
+class PersonModel():
+
+    @classmethod
+    def get_person(cls):
+        try:
+            connection = get_connection()
+            person_list = []
+
+            with connection.cursor() as cursor:
+                cursor.execute('SELECT * FROM person')
+                result_set = cursor.fetchall()
+
+                for row in result_set:
+                    person = person(row[0], row[1], row[2], row[3])
+                    person_list.append(person.to_JSON())
+
+            connection.close()
+            return person_list
+
+        except Exception as ex:
+            raise Exception(str(ex))
--- src/routes/userRoutes.py
+++ src/routes/userRoutes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify
 
 # Models
 from models.usersModel import UserModel
+from models.personModel import PersonModel
 
 
 main=Blueprint('user_blueprint', __name__)
@@ -13,3 +14,13 @@ def get_users():
         return jsonify(Users)
     except Exception as ex:
         return jsonify({'message': str(ex)}),500
+    
+
+@main.route('/person')
+def get_person():
+    try:
+        person = PersonModel.get_person()
+        return jsonify({person})
+    except Exception as ex:
+        return jsonify({'message':str(ex)}); 500
+    
